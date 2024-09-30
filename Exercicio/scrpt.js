4f3e230c8e50faccfcc2e7f1b5671fec328ec94c
Alexandre <lessakalexandre@gmail.com>
2024-09-30 15:59:56 -0300

    Atualizado

---
+++ Exercicio/scrpt.js
@@ -0,0 +1,19 @@
+
+function carregar(){
+    var agora = new Date()
+    var hora = agora.getHours()
+    var msg = window.document.getElementById('msg')
+    var img = document.getElementById('imagem')    
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora < 12) {
+        window.document.body.style.background = '#f3e85091'
+        img.scr = 'Imagens/manha.jpg'
+    }else if(hora >= 12 && hora < 18){
+        window.document.body.style.background = ''
+        img.scr = 'Imagens/tarde.jpg'
+    }else if(hora>18){
+        window.document.body.style.background = 'darkblue'
+        img.scr = 'Imagens/noite.jpg'
+    }
+    
+}
